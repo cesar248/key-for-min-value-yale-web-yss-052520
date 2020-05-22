e4364b96f8b2b34c165e97c7e24da5954511f85b
Cesar Lara <cesar.lara@yale.edu>
2020-05-22 06:16:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value (name_hash)
   low_key = ""
 
   name_hash.each do |key, value|
-    if name_hash[key] < lowest_value 
+    if name_hash[key] < lowest_value
       lowest_value = name_hash[key]
-      low_key = key 
-    end 
-  end 
+      low_key = key
+    end
+  end
 end 
